f5387b3eb4a190c6e807cbcb4d43ba3e2e253b75
Timofey Yefimenko <timaxin@gmail.com>
2020-03-11 21:37:09 +0300

    Improve way of getting icon

--- spoiler/SpoilerUI.js
+++ spoiler/SpoilerUI.js
@@ -1,6 +1,5 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
-import SpoilerIcon from '../../../../../../static/editor-icons/add-subheader.svg';
 
 export default class SpoilerUI extends Plugin {
 
@@ -11,6 +10,7 @@ export default class SpoilerUI extends Plugin {
   init() {
     const editor = this.editor;
 
+    const SpoilerIcon = editor.config.get('spoiler.icon');
     editor.ui.componentFactory.add( 'spoiler', locale => {
       const command = editor.commands.get( 'insertSpoiler' )
 
